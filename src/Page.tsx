e6f1466e3086789c16c3eea2405be84d9b945191
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2023-01-11 11:22:02 +0900

    Feat: add HardCover page flip effect

--- src/Page.tsx
+++ src/Page.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 
 type Props = {
   src: string;
+  cover?: boolean;
 };
 
 const Page = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
   return (
     <div
       className="relative w-full h-full overflow-hidden"
-      data-density="soft"
+      data-density={props.cover ? 'hard' : 'soft'}
       ref={ref}
     >
       <img className="w-full h-full" src={props.src} alt="test" />
